2cf871977aa3833642b8d2c60e209f2255926db9
Thibault Goudouneix <nilmanduil@gmail.com>
2017-11-15 16:46:53 +0100

    Change base URL of API

--- src/services/TrailerAPI.ts
+++ src/services/TrailerAPI.ts
@@ -2,7 +2,7 @@ import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs/Observable";
 
-const API_BASE_URL = 'http://localhost:8080/';
+const API_BASE_URL = 'http://10.1.0.177:8080/';
 const API_TRAILERS = 'trailers/';
 
 @Injectable()
